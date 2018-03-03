10d0e233acfc4cc64082a7719eec90abf687812d
Simon Weis <laguna.ws@gmail.com>
2018-03-03 18:55:38 +0100

    displaying, sorting, filtering alarms

--- greeter.js
+++ greeter.js
@@ -16,11 +16,50 @@ for (var i in resources.resources)
 //parse json file for alarms
 //////////////////////////
 
-// var request = new XMLHttpRequest();
-// request.open("GET", "http://192.168.4.15:8085/alarms", false);
-// request.send(null)
-// var alarms = JSON.parse(request.responseText);
 
+var alarms;
+function getAlarmsForID(id)
+{
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://localhost:8085/alarms?wristId=" + id, false);
+    request.send(null);
+    alarms = JSON.parse(request.responseText);
+}
+
+getAlarmsForID("wrist1Id");
+for (var i in alarms)
+{
+    pushAlarmToList(alarms[i]);
+}
+
+function pushAlarmToList(alarm)
+{
+    var tr = document.createElement("tr");
+    tr.setAttribute("myID", alarm.id);
+    
+    var td = document.createElement("td");
+    td.appendChild(document.createTextNode(alarm.receiverWristId))
+    tr.appendChild(td);
+
+    var td2 = document.createElement("td");
+    td2.appendChild(document.createTextNode(alarm.creatorWristId));
+    tr.appendChild(td2);
+
+    var td3 = document.createElement("td");
+    td3.appendChild(document.createTextNode(" "));
+    tr.appendChild(td3);
+
+    var td4 = document.createElement("td");
+    td4.appendChild(document.createTextNode(alarm.message));
+    tr.appendChild(td4);
+
+    var td5 = document.createElement("td");
+    td5.appendChild(document.createTextNode(alarm.timeWhenShow));
+    tr.appendChild(td5);
+
+    document.getElementById("alarmTable").appendChild(tr);
+
+}
 
 
 /////////////////////////////////////////
@@ -42,6 +81,7 @@ img.onload = function()
 ///////////////////////////////////////////////////////////////
 function newAlarm()
 {
+    //
     overlayOn();
 }
 
@@ -158,7 +198,7 @@ function updateTableSelection(id)
 
 
 ////////////////////////////////////////
-// sort table code  copied from https://www.w3schools.com/howto/howto_js_sort_table.asp
+// sort Resource table code  copied from https://www.w3schools.com/howto/howto_js_sort_table.asp
 ////////////////////////////////////////
 function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
@@ -214,11 +254,68 @@ function sortTable(n) {
       }
     }
   }
+////////////////////////////////////////
+// sort Alarm table code  copied from https://www.w3schools.com/howto/howto_js_sort_table.asp
+////////////////////////////////////////
+function sortTable2(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("alarmTable");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc"; 
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.getElementsByTagName("TR");
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++; 
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
 
 ////////////////////////////////////
-// Filter Table code
+// Filter Resources Table code
 ////////////////////////////////////
-  function myFunction() 
+  function filterResources() 
   {
     // Declare variables 
     var input, filter, table, tr, td, i;
@@ -231,12 +328,13 @@ function sortTable(n) {
     for (i = 0; i < tr.length; i++) 
     {
         var hide =true;
-        td1 = tr[i].getElementsByTagName("td")[0];
-        td2 = tr[i].getElementsByTagName("td")[1];
+        td0 = tr[i].getElementsByTagName("td")[0];
+        td1 = tr[i].getElementsByTagName("td")[1];
+        td2 = tr[i].getElementsByTagName("td")[2];
 
-        if (td1 && td2) 
+        if (td0 && td1 && td2) 
         {
-            if (td1.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1) 
+            if (td0.innerHTML.toUpperCase().indexOf(filter) > -1 || td1.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1) 
             {
                 tr[i].style.display = "";    
             } 
@@ -246,4 +344,40 @@ function sortTable(n) {
             }
         } 
     }
+}
+
+////////////////////////////////////
+// Filter Alarms code
+////////////////////////////////////
+function filterAlarms() 
+{
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("alarmFilter");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("alarmTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) 
+  {
+      var hide =true;
+      td0 = tr[i].getElementsByTagName("td")[0];
+      td1 = tr[i].getElementsByTagName("td")[1];
+      td2 = tr[i].getElementsByTagName("td")[2];
+      td3 = tr[i].getElementsByTagName("td")[3];
+
+      if (td0 && td1 && td2 && td3) 
+      {
+          if (  td0.innerHTML.toUpperCase().indexOf(filter) > -1 || td1.innerHTML.toUpperCase().indexOf(filter) > -1|| 
+                td2.innerHTML.toUpperCase().indexOf(filter) > -1 || td3.innerHTML.toUpperCase().indexOf(filter) > -1) 
+          {
+              tr[i].style.display = "";    
+          } 
+          else
+          {
+              tr[i].style.display = "none";
+          }
+      } 
+  }
 }
