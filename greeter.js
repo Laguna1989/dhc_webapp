be1eb1d2e743b855dfc95bb9df07d4ee5b5fe9c7
Simon Weis <laguna.ws@gmail.com>
2018-03-03 12:44:09 +0100

    clicking a table results in the correct ID

--- greeter.js
+++ greeter.js
@@ -7,6 +7,7 @@ var request = new XMLHttpRequest();
 request.open("GET", "resources.json", false);
 request.send(null)
 var resources = JSON.parse(request.responseText);
+
 for (var i in resources.resources)
 {
    newElement(resources.resources[i]);
@@ -27,14 +28,13 @@ for (var i in resources.resources)
 /////////////////////////////////////////
 var c = document.getElementById("mapCanvas");
 var ctx = c.getContext("2d");
+
 var img = new Image();
-img.src = "map.png"; // can also be a remote URL e.g. http://
-img.onload = function() {
-   ctx.drawImage(img,0,0);
-};
-
-
-
+img.src = "map2.png"; // can also be a remote URL e.g. http://
+img.onload = function() 
+{
+    ctx.drawImage(img,0,0);
+};  // can be redrawn later, but loading has to be complete first
 
 
 
@@ -43,7 +43,7 @@ img.onload = function() {
 function newElement(resource) 
 {
     var tr = document.createElement("tr");
-    
+    tr.setAttribute("myID", resource.id);
     
     var td = document.createElement("td");
     td.appendChild(document.createTextNode(resource.type));
@@ -56,11 +56,48 @@ function newElement(resource)
 
     document.getElementById("resourceTable").appendChild(tr);
 
+}
+
+//////////////////////////////
+// clicking the table
+//////////////////////////////
+var table = document.getElementById("resourceTable");
+if (table != null) 
+{
     
-    for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
+    for (var i = 0; i < table.rows.length; i++) 
+    {
+        table.rows[i].onclick = function () 
+        {
+            var myID = this.getAttribute("myID") ;
+            updateTableSelection(myID);
+        };
     }
+}
+
+
+/////////////////////////////////////////
+// load json mock positions
+/////////////////////////////////////////
+// TODO
+function getPositionByID(id)
+{
+    if (id == 0)
+    {
+        // TODO get position data
+        return [5,5];
     }
+    else
+    {
+        return [25,25];
+    }
+}
+
+function updateTableSelection(id) 
+{
+    alert(id);
+    ctx.clearRect(0,0,800,600);
+    ctx.drawImage(img,0,0);
+    var pos = getPositionByID(id);
+    ctx.fillRect(pos[0], pos[1], 20, 20);
 }
