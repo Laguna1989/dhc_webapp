dc86f237be9768752753cba6fce89a01b21eef62
Simon Weis <laguna.ws@gmail.com>
2018-03-03 13:45:22 +0100

    searching table works now

--- greeter.js
+++ greeter.js
@@ -171,3 +171,36 @@ function sortTable(n) {
       }
     }
   }
+
+////////////////////////////////////
+// Filter Table code
+////////////////////////////////////
+  function myFunction() 
+  {
+    // Declare variables 
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("filterInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("resourceTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) 
+    {
+        var hide =true;
+        td1 = tr[i].getElementsByTagName("td")[0];
+        td2 = tr[i].getElementsByTagName("td")[1];
+
+        if (td1 && td2) 
+        {
+            if (td1.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1) 
+            {
+                tr[i].style.display = "";    
+            } 
+            else
+            {
+                tr[i].style.display = "none";
+            }
+        } 
+    }
+}
